6f3e1e527baabaacd50d4ba69971a8f6c0963e83
ericksitocg <ericksitocg@hotmail.com>
2019-08-07 04:19:00 -0500

    configurado con base y creacion de migrations sin foreign key

---
+++ database/migrations/2019_08_07_084603_create_estados_pedidos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstadosPedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estadosPedidos', function (Blueprint $table) {
+            $table->bigIncrements('estadoPedido_id');
+            $table->string('estadoPedido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estadosPedidos');
+    }
+}
